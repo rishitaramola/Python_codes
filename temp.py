1eb92475e7798f7e7677f995a27ff507a363cade
rishitaramola <rishitaramola004@gmail.com>
2025-08-30 02:16:48 +0530

    temp

---
+++ temp.py
@@ -0,0 +1,4 @@
+#celsius to farenheit conversion
+celsius = float(input("Enter temperature in Celsius: "))
+farenheit = (celsius * 9/5) + 32
+print(f"{celsius} Celsius is equal to {farenheit} Farenheit")
